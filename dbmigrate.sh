9504455429a9d45a940704d97fb21089168900a2
anildhungana <59248969+anildhungana@users.noreply.github.com>
2020-01-07 03:33:38 -0500

    Update dbmigrate.sh

--- dbmigrate.sh
+++ dbmigrate.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 dbsize=$(mysql -u anil -panil1234 -h 10.13.198.63 -Bse 'SELECT table_schema AS "Database",(SUM(data_length + index_length)) AS "Size(MB)" FROM information_schema.TABLES where table_schema="classicmodels";'| grep -o '[0-9]*')
-dbsizel=$(df -k '/home/osboxes/Desktop')
+dbsizel=$(df -k '/home/osboxes/Desktop') #check size of path that to be imported on
 NOW=$(date +"%m-%d-%y")
-if ($dbsize1>$dbsize)
+if ($dbsize1>$dbsize) #Check and compare the size
 then
 	echo Database Imported Successfully ON $NOW
 	mysqldump -u anil -panil1234 -h10.13.198.63 classicmodels >'/home/osboxes/Desktop/importdb.sql'
